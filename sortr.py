a7f0b42afa88b4ab4957cbf4661268ecd3d55a61
Israel Adura <israeladura@gmail.com>
2018-07-01 17:03:35 +0100

    added the python magic module

--- sortr.py
+++ sortr.py
@@ -2,6 +2,10 @@ from itertools import chain
 from os import listdir, walk
 from os.path import expanduser, isfile, join
 
+# import the python magic module using pip install python-magic
+import magic
+
+mime = magic.Magic(mime=True)
 # get home dir
 homeDir = expanduser("~")
 
@@ -12,7 +16,15 @@ downloadsDir = '%s/Downloads/test' % (homeDir)
 
 # get all files in the downloads directory
 for root, dirs, files in walk(downloadsDir):
-    #loop through all the files
+    # loop through all the files
     for file in files:
-        if(isfile('%s/%s' % (downloadsDir, file))):
+        # print(file)
+        file = '%s/%s' % (downloadsDir, file)
+        if(isfile(file)):
             print(file)
+            mime.from_file(file)
+
+# sort function
+
+
+# def sort(fileType, directory):
