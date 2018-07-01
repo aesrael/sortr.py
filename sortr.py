6e320dd22d4b957d1d5d435e00413b0ecb444ef7
Israel Adura <israeladura@gmail.com>
2018-07-02 00:45:11 +0100

    sort working

--- sortr.py
+++ sortr.py
@@ -8,24 +8,41 @@ mimetypes.init()
 # get home dir
 homeDir = expanduser("~")
 
-# directories
-dirs = ['Downloads', 'Music', 'Desktop', 'Videos', 'Pictures', 'Documents']
-
 downloadsDir = '%s/Downloads/test' % (homeDir)
 
+# folders to sort to
+music = '%s/Music' % (homeDir)
+video = '%s/Video' % (homeDir)
+pictures = '%s/Pictures' % (homeDir)
+documents = '%s/Documents' % (homeDir)
 
-def sort(file):
-    if mimetypes.guess_extension(file) == ('application/pdf', None):
-        rename(file)
+def sort(exts, dst):
+    # get all files in the downloads directory
+    for root, dirs, files in walk(downloadsDir):
+        # loop through all the files
+        for file in files:
+            path = join(root, file)
+            filePath = path
+            # check if it is a file
+            if(isfile(filePath)):
+                for ext in exts:
+                    if filePath.find(ext) > -1:
+                        rename(filePath, dst)
+                        print('%s found' % (file))
+                        # print(filePath,dst)
 
 
-# get all files in the downloads directory
-for root, dirs, files in walk(downloadsDir):
-    # loop through all the files
-    for file in files:
-        path = join(root, file)
-        filePath = path
-        # check if it is a file
-        if(isfile(filePath)):
-            print mimetypes.guess_type(filePath)
-            sort(filePath)
+# sort mp3 files
+sort(['mp3', 'ogg', 'wav'], music)
+# sort video files
+sort(['mp4', 'avi', 'flv', 'vob', 'mpg', 'mpeg'], video)
+# sort books
+sort(['pdf', 'epub'], documents)
+# sort zip, gzip and rar files
+sort(['zip', 'gzip', 'rar'], documents)
+# sort apps
+sort(['dmg', 'exe'], documents)
+# sort pictures
+sort(['png', 'jpeg', 'jpg'], pictures)
+# sort documents
+sort(['doc', 'ppt'], documents)
