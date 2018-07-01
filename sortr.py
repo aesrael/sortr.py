df0d870c546f39f7171ce39879c1a4ef9fc8e881
Israel Adura <israeladura@gmail.com>
2018-07-01 16:32:01 +0100

    get files with os.walk

--- sortr.py
+++ sortr.py
@@ -1,10 +1,18 @@
-from os import listdir
-from os.path import expanduser
-home = expanduser("~")
-# from os.path import isfile, join
+from itertools import chain
+from os import listdir, walk
+from os.path import expanduser, isfile, join
 
-# print(listdir())
+# get home dir
+homeDir = expanduser("~")
 
-home = expanduser("~")
-downloadsDir=home
-print(listdir(home)
+# directories
+dirs = ['Downloads', 'Music', 'Desktop', 'Videos', 'Pictures', 'Documents']
+
+downloadsDir = '%s/Downloads/test' % (homeDir)
+
+# get all files in the downloads directory
+for root, dirs, files in walk(downloadsDir):
+    #loop through all the files
+    for file in files:
+        if(isfile('%s/%s' % (downloadsDir, file))):
+            print(file)
