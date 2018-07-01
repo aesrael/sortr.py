2f5d2587f5f445fbecf23d6fed847cc855ec67e0
Israel Adura <israeladura@gmail.com>
2018-07-01 15:14:43 +0100

    initial commit

---
+++ sortr.py
@@ -0,0 +1,10 @@
+from os import listdir
+from os.path import expanduser
+home = expanduser("~")
+# from os.path import isfile, join
+
+# print(listdir())
+
+home = expanduser("~")
+downloadsDir=home
+print(listdir(home)
